e4dcf73ff5d3cc7f9204f1a64d925c50508a21ce
mhmdho <hadiomidm@gmail.com>
2022-09-16 16:13:53 +0430

    Test and run fastapi Documents for api, database and websockets

---
+++ mainApi.py
@@ -0,0 +1,36 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from utils import binanceDataTime
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/{symbol}/{interval}/{startTime}")
+def read_data(symbol: str, interval: str, startTime: str, endTime: Union[str, None] = None):
+    print('---------', endTime)
+    data = binanceDataTime(symbol, interval, startTime, endTime).get_data()
+    return data
