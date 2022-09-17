b21fba79cf2c85159b86fb7450392923d66cdfdd
mhmdho <hadiomidm@gmail.com>
2022-09-17 18:44:04 +0430

    Add atctic database

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from typing import Union
 from fastapi import FastAPI
 
 from utils import binanceDataTime
+from arctic import Arctic
 
+
+# Connect to Local MONGODB
+store = Arctic('localhost')
 
 app = FastAPI()
 
@@ -10,4 +14,11 @@ app = FastAPI()
 @app.get("/{symbol}/{interval}/{startTime}")
 def read_data(symbol: str, interval: str, startTime: str, endTime: Union[str, None] = None):
     data = binanceDataTime(symbol, interval, startTime, endTime).get_data()
-    return data
+    
+    store.initialize_library(symbol)
+    library = store[symbol]
+    library.write(interval, data, prune_previous_version=True)
+
+    print('------all lib----',library.list_symbols())
+    return library.read(interval).data
+
